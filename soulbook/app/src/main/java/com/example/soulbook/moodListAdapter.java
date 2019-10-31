78004e3e72ac405df31fc745d7bdd90818b236d4
tsztszw <43255164+tsztszw@users.noreply.github.com>
2019-10-30 19:35:57 -0600

    1

--- soulbook/app/src/main/java/com/example/soulbook/moodListAdapter.java
+++ soulbook/app/src/main/java/com/example/soulbook/moodListAdapter.java
@@ -1,19 +1,24 @@
 package com.example.soulbook;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
+import com.example.soulbook.ui.home.HomeFragment;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,20 +29,23 @@ import java.util.ArrayList;
 public class moodListAdapter extends BaseAdapter {
     private Context context;
     ArrayList<mood> moods;
-    ArrayList<String> nicknames, hours;
+    ArrayList<String> nicknames, moodId;
     private LayoutInflater mLayoutInflater;
     private ImageView listViewAvatar;
     private TextView listViewNickName, listViewMoodText, listViewTime, listViewLocation, listViewLikeList;
     private ImageButton deleteButton, likeButton;
     private String nickname;
+    private HomeFragment m;
 
-    public moodListAdapter(Context context, ArrayList<mood> moods, ArrayList<String> nicknames){
+    public moodListAdapter(Context context, ArrayList<mood> moods, ArrayList<String> nicknames, ArrayList<String> moodId, HomeFragment m){
+        this.m = m;
         this.context = context;
         mLayoutInflater = LayoutInflater.from(context);
         this.moods = moods;
         this.nicknames = nicknames;
-        this.hours = hours;
+        this.moodId = moodId;
     }
+
     @Override
     public int getCount() {
         return moods.size();
@@ -54,7 +62,7 @@ public class moodListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         convertView = mLayoutInflater.inflate(R.layout.mood_view_content, null);
         listViewAvatar = convertView.findViewById(R.id.listview_avatar);
         listViewNickName = convertView.findViewById(R.id.listview_nickname);
@@ -64,11 +72,25 @@ public class moodListAdapter extends BaseAdapter {
         listViewLikeList = convertView.findViewById(R.id.listview_like_list);
         deleteButton = convertView.findViewById(R.id.listview_delete_mood);
         likeButton = convertView.findViewById(R.id.listview_like_button);
-        mood thismood = moods.get(position);
+        final mood thismood = moods.get(position);
+        final String Id = moodId.get(position);
+        if (thismood.getPoster().equals(datasave.UserId)){
+            deleteButton.setVisibility(View.VISIBLE);
+        }
+        else{
+            deleteButton.setVisibility(View.INVISIBLE);
+        }
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datasave a = new datasave();
+                m.removemood(position);
+            }
+        });
         listViewNickName.setText(nicknames.get(position));
         listViewMoodText.setText(thismood.getContent());
         listViewTime.setText(thismood.getTime().printTime());
         return convertView;
-
     }
+
 }
